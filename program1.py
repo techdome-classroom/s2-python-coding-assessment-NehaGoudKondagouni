5bd0b5b70cbc495d7596d9431b85160424932f36
NehaGoudKondagouni <151984990+NehaGoudKondagouni@users.noreply.github.com>
2024-11-11 15:26:09 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,20 +1,21 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
-
-
-
-
-
-
+        stack = []
     
+        dict1 = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in dict1:
+                top = stack.pop() if stack else '#'
+                
+                if dict1[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
 
 
 
-  
+
 
