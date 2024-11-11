1c37211a4df1422705af94f74e5233993ed57548
NehaGoudKondagouni <151984990+NehaGoudKondagouni@users.noreply.github.com>
2024-11-11 15:26:36 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,21 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    
+        total = 0
+        length = len(s)
+        
+        for i in range(length):
+            current_value = roman_dict[s[i]]
+            
+            if i + 1 < length and current_value < roman_dict[s[i + 1]]:
+                total -= current_value
+            else:
+                total += current_value
+        
+        return total
 
 
 
